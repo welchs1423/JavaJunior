5db62f5cba685804fc70730a47c82106cca65c93
welchs1423 <welchs1423@naver.com>
2023-11-19 01:14:27 +0900

    [자바 기초 강의] 71강. 6.14 Getter와 Setter

---
+++ src/ch06/sec14/CarExample.java
@@ -0,0 +1,17 @@
+package ch06.sec14;
+
+public class CarExample {
+	public static void main(String[] args) {
+		Car myCar = new Car();
+
+		myCar.setSpeed(-50);
+		System.out.println(myCar.getSpeed());
+
+		myCar.setSpeed(60);
+		System.out.println(myCar.getSpeed());
+
+		if (myCar.isStop()) {
+			myCar.setStop(true);
+		}
+	}
+}

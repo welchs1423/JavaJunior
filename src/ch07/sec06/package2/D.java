a6a220d0e350429e6f487c59c5f0e06d1b629ffc
welchs1423 <welchs1423@naver.com>
2023-11-22 21:44:02 +0900

    [자바 기초 강의] 78강. 7.6 protected 접근 제한자

---
+++ src/ch07/sec06/package2/D.java
@@ -0,0 +1,25 @@
+package ch07.sec06.package2;
+
+import ch07.sec06.package1.A;
+
+public class D extends A {
+	// 생성자 선언
+	public D() {
+		super();
+	}
+
+	// 메소드 선언
+	public void method() {
+		// A 필드값 변경
+		this.field = "value";
+		// A메소드 호출
+		this.method();
+	}
+
+	// 메소드 선언
+	public void method2() {
+		A a = new A();
+		a.field = "value";
+		a.method();
+	}
+}

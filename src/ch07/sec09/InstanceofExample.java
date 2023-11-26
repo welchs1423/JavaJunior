e1bbab499045af0264645a4992cff9fae36ca172
welchs1423 <welchs1423@naver.com>
2023-11-26 17:53:49 +0900

    [자바 기초 강의] 82강. 7.10 추상 클래스

--- src/ch07/sec09/InstanceofExample.java
+++ src/ch07/sec09/InstanceofExample.java
@@ -23,16 +23,17 @@ public class InstanceofExample {
 				student.study();
 			}
 			
-			public static void main(String[] args) {
-				//Person 객체를 매게값으로 제공하고 personInfo() 메소드 호출
-				Person p1 = new Person("홍길동");
-				personInfo(p1);
-				
-				System.out.println();
-				
-				//Student 객체를 매게값으로 제공하고 personInfo() 메소드 호출
-				Person p2 = new Student("김길동",10);
-				personInfo(p2);
-			}
+		}
+	
+		public static void main(String[] args) {
+			//Person 객체를 매게값으로 제공하고 personInfo() 메소드 호출
+			Person p1 = new Person("홍길동");
+			personInfo(p1);
+			
+			System.out.println();
+			
+			//Student 객체를 매개값으로 제공하고 personInfo() 메소드 호출
+			Person p2 = new Student("김길동", 10);
+			personInfo(p2);
 		}
 }
--- src/ch07/sec09/Person.java
+++ src/ch07/sec09/Person.java
@@ -5,4 +5,12 @@ public class Person {
 	public String name;
 	
 	//생성자 선언
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	//메소드 선언
+	public void walk() {
+		System.out.println("걷습니다.");
+	}
 }
